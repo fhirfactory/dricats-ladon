c48bdf91cb1eed530b419351a18e884d58bfb36f
Mark Hunter <m.a.hunter@outlook.com>
2021-09-07 14:11:27 +1000

    Remove Twin Based Workflow

--- pegacorn-ladon-twin-behaviours/src/main/java/net/fhirfactory/pegacorn/ladon/behaviours/archetypes/BusinessUnitCentricTimerBasedBehaviour.java
+++
@@ -1,6 +0,0 @@
-package net.fhirfactory.pegacorn.ladon.behaviours.archetypes;
-
-import net.fhirfactory.pegacorn.ladon.behaviours.archetypes.common.GenericTimerBasedBehaviour;
-
-abstract public class BusinessUnitCentricTimerBasedBehaviour extends GenericTimerBasedBehaviour {
-}
